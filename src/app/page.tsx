b55246cd0e052535ff21cc3fa9fae145eb8b9307
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-05-26 09:14:37 +0530

    added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner";
 import MainSlider from "@/components/MainSlider";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import FeaturedProducts from "@/components/Products/FeaturedProducts";
@@ -6,6 +7,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default async function Home() {
   const qc = new QueryClient();
@@ -21,6 +23,56 @@ export default async function Home() {
       <MaxWidthWrapper>
         {/* Featured Products */}
         <FeaturedProducts />
+
+        {/* Banners */}
+
+        <section className="grid lg:grid-cols-3 gap-x-5 gap-y-5 text-white my-4">
+          <Banner
+            imgUrl="/images/banner-01.jpg"
+            className="h-56 w-auto rounded-lg cursor-pointer"
+          >
+            <span className="px-2 py-1 text-xs font-semibold rounded-xl bg-primary">
+              On Sale This Week
+            </span>
+            <h3 className="my-3 text-2xl font-bold max-w-60 capitalize">
+              The World&apos;s Best Engine Oils for Your Car
+            </h3>
+
+            <Link href="#" className="text-sm">
+              Shop Now
+            </Link>
+          </Banner>
+          <Banner
+            imgUrl="/images/banner-02.jpg"
+            className="h-56 w-auto rounded-lg cursor-pointer"
+          >
+            <span className="px-2 py-1 text-xs font-semibold rounded-xl bg-primary">
+              On Sale This Week
+            </span>
+            <h3 className="my-3 text-2xl font-bold max-w-60 capitalize">
+              Change tires for winter to ensure your safety.
+            </h3>
+
+            <Link href="#" className="text-sm">
+              Shop Now
+            </Link>
+          </Banner>
+          <Banner
+            imgUrl="/images/banner-03.jpg"
+            className="h-56 w-auto rounded-lg cursor-pointer"
+          >
+            <span className="px-2 py-1 text-xs font-semibold rounded-xl bg-primary">
+              On Sale This Week
+            </span>
+            <h3 className="my-3 text-2xl font-bold max-w-60 capitalize">
+              unlish the true potential of your vehicle
+            </h3>
+
+            <Link href="#" className="text-sm">
+              Shop Now
+            </Link>
+          </Banner>
+        </section>
       </MaxWidthWrapper>
     </HydrationBoundary>
   );
