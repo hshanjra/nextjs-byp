ab7cab7241aee20e5555e227f154bc827361376e
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-06-04 15:57:35 +0530

    fix: account header

--- src/components/AccountHeader.tsx
+++ src/components/AccountHeader.tsx
@@ -8,7 +8,8 @@ export default function AccountHeader() {
   return (
     <div className="flex items-center justify-between">
       <h3 className="text-xl font-semibold my-5">
-        {userData ? `${userData.lastName}'s` : "Your"} Account
+        {userData ? `${userData.firstName}" "${userData.lastName}'s` : "Your"}{" "}
+        Account
       </h3>
       <Button
         variant="link"
