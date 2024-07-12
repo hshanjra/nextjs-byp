9af39d31ce79874f86a03747abe6f15a5511d917
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-07-12 22:32:31 +0530

    update CheckoutForm.tsx

--- src/components/Forms/CheckoutForm.tsx
+++ src/components/Forms/CheckoutForm.tsx
@@ -760,7 +760,7 @@ export default function CheckoutForm({
                                         fontSmoothing: "antialiased",
                                         ":-webkit-autofill": {
                                           color: "black",
-                                          backgroundColor: "white",
+                                          backgroundColor: "white !important",
                                         },
                                         "::placeholder": {
                                           color: "#2b2b2b9c",
