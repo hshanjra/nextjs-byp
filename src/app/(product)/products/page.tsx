a8382b07ae82085e96c3c7647acab6be61a17c1d
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-07-15 17:11:33 +0530

    update page.tsx and ProductHoverInfoCard.tsx

--- src/app/(product)/products/page.tsx
+++ src/app/(product)/products/page.tsx
@@ -6,7 +6,7 @@ export default async function ProductsPage() {
   const products: Product[] = await getAllProducts({ limit: 20 });
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-2">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-2">
       {products &&
         products.map((product: Product) => (
           <ProductHoverInfoCard product={product} key={product.productId} />
