17b9662547178f84f2dcebaba1e9f2b5b9ba7134
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-07-16 15:34:05 +0530

    update page.tsx

--- src/app/(product)/products/featured/page.tsx
+++ src/app/(product)/products/featured/page.tsx
@@ -1,3 +1,18 @@
-export default function FeaturedProductsPage() {
-  return <div>FeaturedProductsPage</div>;
+import { getAllProducts } from "@/actions/ProductsAction";
+import ProductHoverInfoCard from "@/components/Products/ProductHoverInfoCard";
+import { Product } from "@/types/product";
+
+export default async function FeaturedProductsPage() {
+  // TODO: modify this to fetch only featured products
+  const products: Product[] = await getAllProducts({ limit: 20 });
+
+  return (
+    products && (
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-2">
+        {products.map((product: Product) => (
+          <ProductHoverInfoCard product={product} key={product.productId} />
+        ))}
+      </div>
+    )
+  );
 }
