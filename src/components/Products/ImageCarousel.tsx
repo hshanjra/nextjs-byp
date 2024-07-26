8492e83e030867b28c9eb87dc9ada96e427078a5
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-07-26 16:38:43 +0530

    update ImageCarousel.tsx

--- src/components/Products/ImageCarousel.tsx
+++ src/components/Products/ImageCarousel.tsx
@@ -58,7 +58,7 @@ export default function ImageCarousel({
                     alt={image.alt}
                     width={800}
                     height={800}
-                    className="w-full h-[500px] md:h-[600px] object-cover transition-transform duration-300 cursor-zoom-in"
+                    className="w-full h-[400px] md:h-[600px] object-scale-down transition-transform duration-300 cursor-zoom-in"
                     style={index === selectedIndex ? zoomStyle : {}}
                     onMouseMove={(e) => handleMouseMove(e, index)}
                     onMouseLeave={handleMouseLeave}
