9f0370676eafc7d9a3618b651c9632724a279fc3
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-08-01 21:35:11 +0530

    chore: delete loading.tsx

--- src/app/(categories)/categories/[slug]/loading.tsx
+++ src/app/(categories)/categories/[slug]/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function loading() {
   return (
-    <div className="mx-auto mt-10">
+    <div className="mx-auto my-10">
       <div className="flex flex-col items-center gap-2">
         <Loader2 className="h-8 w-8 animate-spin text-primary" />
         <h3 className="font-semibold text-xl">Loading...</h3>
