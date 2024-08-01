f4bf99a5f6a4d11bd0ee7b9202bceeca81f6aa85
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-08-01 21:33:51 +0530

    delete 1 file and update 1 file

--- src/app/(categories)/categories/page.tsx
+++ src/app/(categories)/categories/page.tsx
@@ -1,4 +1,5 @@
 import { getAllCategories } from "@/actions/CategoryAction";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import EmptyState from "@/components/Products/EmptyState";
 import { SITE_METADATA } from "@/constants";
 import { Metadata } from "next";
@@ -19,33 +20,37 @@ export default async function CategoriesPage() {
   if (!categories || error) return <EmptyState />;
 
   return (
-    <section>
-      <h1 className="text-3xl uppercase font-bold mb-5">Explore Categories</h1>
-      {categories && categories.length > 0 ? (
-        <div className="grid grid-cols-2 lg:grid-cols-4">
-          {categories.map((cat) => (
-            <div
-              key={cat._id}
-              className="overflow-hidden w-full border p-5 hover:scale-105 hover:shadow-xl transition-all duration-200 cursor-pointer bg-white"
-            >
-              <Link href={`/categories/${cat.categorySlug}`}>
-                <Image
-                  src={cat.categoryThumbnail}
-                  width={100}
-                  height={100}
-                  alt={cat.categoryName}
-                  className="w-auto h-auto m-auto"
-                />
-                <p className="text-center text-sm text-zinc-500">
-                  {cat.categoryName}
-                </p>
-              </Link>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <EmptyState />
-      )}
-    </section>
+    <MaxWidthWrapper>
+      <section className="my-5">
+        <h1 className="text-3xl uppercase font-bold my-5">
+          Explore Categories
+        </h1>
+        {categories && categories.length > 0 ? (
+          <div className="grid grid-cols-2 lg:grid-cols-4">
+            {categories.map((cat) => (
+              <div
+                key={cat._id}
+                className="overflow-hidden w-full border p-5 hover:scale-105 hover:shadow-xl transition-all duration-200 cursor-pointer bg-white"
+              >
+                <Link href={`/categories/${cat.categorySlug}`}>
+                  <Image
+                    src={cat.categoryThumbnail}
+                    width={100}
+                    height={100}
+                    alt={cat.categoryName}
+                    className="w-auto h-auto m-auto"
+                  />
+                  <p className="text-center text-sm text-zinc-500">
+                    {cat.categoryName}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <EmptyState />
+        )}
+      </section>
+    </MaxWidthWrapper>
   );
 }
