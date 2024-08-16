47319d3046be38346b93bf3caa3cff365812a0e6
Hanspreet Singh <hshanjra.dev@gmail.com>
2024-08-16 17:24:11 +0530

    update page.tsx and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={cn("relative antialiased", krub.className)}>
         <Suspense>
           <QueryProvider>
